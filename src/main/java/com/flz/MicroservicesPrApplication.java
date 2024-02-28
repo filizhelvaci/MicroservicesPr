c1402df3a3f074d621919b5eeea0fa51ac6d1122
filizhelvaci <filizhelvaci82@gmail.com>
2024-02-28 08:41:20 +0300

    Version1
    AuthService created.

---
+++ src/main/java/com/flz/MicroservicesPrApplication.java
@@ -0,0 +1,13 @@
+package com.flz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesPrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesPrApplication.class, args);
+	}
+
+}
